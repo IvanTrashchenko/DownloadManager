49ffd0058db7f592b1860b06c87b8a3f7f7256c5
Ivan Trashchenko <ivan.trashchenko@quibiq.ch>
2022-06-20 17:51:19 +0300

    App: Add layout

--- DownloadManager.App/Main.Designer.cs
+++ DownloadManager.App/Main.Designer.cs
@@ -30,23 +30,123 @@ namespace DownloadManager.App
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtFileUrl = new System.Windows.Forms.TextBox();
+            this.lblDownloadMethod = new System.Windows.Forms.Label();
+            this.lblDestinationFolder = new System.Windows.Forms.Label();
+            this.lblFileUrl = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.txtFileUrl, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblDownloadMethod, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblDestinationFolder, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblFileUrl, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(35, 3);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(35);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(850, 232);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtFileUrl
+            // 
+            this.txtFileUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileUrl.Location = new System.Drawing.Point(0, 38);
+            this.txtFileUrl.Margin = new System.Windows.Forms.Padding(0);
+            this.txtFileUrl.Name = "txtFileUrl";
+            this.txtFileUrl.Size = new System.Drawing.Size(850, 23);
+            this.txtFileUrl.TabIndex = 5;
+            this.txtFileUrl.Text = "https://i.redd.it/ufxj58wy79w81.jpg";
+            // 
+            // lblDownloadMethod
+            // 
+            this.lblDownloadMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDownloadMethod.AutoSize = true;
+            this.lblDownloadMethod.Location = new System.Drawing.Point(0, 175);
+            this.lblDownloadMethod.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDownloadMethod.Name = "lblDownloadMethod";
+            this.lblDownloadMethod.Size = new System.Drawing.Size(106, 15);
+            this.lblDownloadMethod.TabIndex = 4;
+            this.lblDownloadMethod.Text = "Download method";
+            // 
+            // lblDestinationFolder
+            // 
+            this.lblDestinationFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDestinationFolder.AutoSize = true;
+            this.lblDestinationFolder.Location = new System.Drawing.Point(0, 99);
+            this.lblDestinationFolder.Margin = new System.Windows.Forms.Padding(0);
+            this.lblDestinationFolder.Name = "lblDestinationFolder";
+            this.lblDestinationFolder.Size = new System.Drawing.Size(101, 15);
+            this.lblDestinationFolder.TabIndex = 3;
+            this.lblDestinationFolder.Text = "Destination folder";
+            // 
+            // lblFileUrl
+            // 
+            this.lblFileUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFileUrl.AutoSize = true;
+            this.lblFileUrl.Location = new System.Drawing.Point(0, 23);
+            this.lblFileUrl.Margin = new System.Windows.Forms.Padding(0);
+            this.lblFileUrl.Name = "lblFileUrl";
+            this.lblFileUrl.Size = new System.Drawing.Size(49, 15);
+            this.lblFileUrl.TabIndex = 2;
+            this.lblFileUrl.Text = "File URL";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(35, 238);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(850, 447);
+            this.txtResult.TabIndex = 9;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 741);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(550, 550);
             this.Name = "Main";
             this.Padding = new System.Windows.Forms.Padding(32);
             this.Text = "DownloadManager";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblFileUrl;
+        private System.Windows.Forms.Label lblDestinationFolder;
+        private System.Windows.Forms.Label lblDownloadMethod;
+        private System.Windows.Forms.TextBox txtFileUrl;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
 
