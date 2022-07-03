aa0aaccf112293375bd68ae35ff286a96678e7cf
Ivan Trashchenko <ivan.trashchenko@quibiq.ch>
2022-07-03 03:39:21 +0300

    Database: Initial commit

---
+++ DownloadManager.Database/Table/File.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[File]
+(
+	[FileId] INT NOT NULL IDENTITY, 
+    [FileName] NVARCHAR(256) NOT NULL,
+	[FileDownloadDirectory] NVARCHAR(MAX) NOT NULL, 
+    [FileDownloadMethod] INT NOT NULL, 
+    [FileDownloadTime] DATETIME2 NOT NULL, 
+    CONSTRAINT [PK_File] PRIMARY KEY ([FileId])
+)
