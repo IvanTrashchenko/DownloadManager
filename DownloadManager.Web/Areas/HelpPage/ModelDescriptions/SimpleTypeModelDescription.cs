326207b58e2937b690525f08f4693a3c6995e497
qbq-itr <ivan.trashchenko@quibiq.ch>
2023-04-04 17:27:22 +0300

    DownloadManager.Web: Initial commit

---
+++ DownloadManager.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DownloadManager.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ DownloadManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DownloadManager.Web.WebApiApplication" Language="C#" %>
