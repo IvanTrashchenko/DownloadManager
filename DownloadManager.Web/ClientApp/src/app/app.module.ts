bcdf2a52e8d67b84aad94121f69c5f3a53cad2c2
qbq-itr <ivan.trashchenko@quibiq.ch>
2023-06-29 16:42:30 +0300

    ClientApp: Reorganize modules

--- DownloadManager.Web/ClientApp/src/app/app.module.ts
+++ DownloadManager.Web/ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
